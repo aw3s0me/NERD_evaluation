bc8568f7659d057f5fbd9fe174e4d221f477f64a
aw3s0me <akorovin00@gmail.com>
2017-02-20 22:32:51 +0100

    basic stanford ner component

---
+++ src/main/java/components/stanford/StanfordNERComponent.java
@@ -0,0 +1,37 @@
+package components.stanford;
+
+import components.NERDComponent;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+
+import java.util.Properties;
+
+/**
+ * Created by akorovin on 20.02.2017.
+ */
+public class StanfordNERComponent implements NERDComponent {
+    private StanfordCoreNLP pipeline;
+
+    public StanfordNERComponent() {
+        Properties props = new Properties();
+        props.put("annotators", "tokenize, ssplit, pos, lemma, ner");
+        // Create a new pipeline
+        this.pipeline = new StanfordCoreNLP(props);
+    }
+
+    public void process(String slide) {
+        // Create an empty annotation just with the given text
+        Annotation document = new Annotation(slide);
+        // Run the stanford annotator on question
+        pipeline.annotate(document);
+        // TODO: analyze info. run calculation of metrics
+    }
+
+    public Double calculatePrecision() {
+        return null;
+    }
+
+    public Double calculateRecall() {
+        return null;
+    }
+}
