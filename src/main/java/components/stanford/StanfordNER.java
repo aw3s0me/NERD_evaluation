b582190cfb08f6e141d45eec2f5d3fcea7c8cf5f
aw3s0me <akorovin00@gmail.com>
2017-02-21 16:19:06 +0100

    stanford ner class

--- src/main/java/components/stanford/StanfordNER.java
+++ src/main/java/components/stanford/StanfordNER.java
@@ -1,9 +1,15 @@
 package components.stanford;
 
 import components.NER;
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.trees.TreeCoreAnnotations;
+import edu.stanford.nlp.util.CoreMap;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
@@ -25,8 +31,31 @@ public class StanfordNER implements NER {
         Annotation document = new Annotation(slide);
         // Run the stanford annotator on question
         pipeline.annotate(document);
-        // TODO: analyze info. run calculation of metrics
+        // TODO: run calculation of metrics
 
-        return null;
+        return interpretOutput(document);
+    }
+
+    private List<String> interpretOutput(Annotation document) {
+        List<CoreMap> sentences = document.get(CoreAnnotations.SentencesAnnotation.class);
+        List<String> res = new ArrayList<String>();
+
+        for (CoreMap sentence: sentences) {
+            for (CoreLabel token: sentence.get(CoreAnnotations.TokensAnnotation.class)) {
+                String word = token.get(CoreAnnotations.TextAnnotation.class);
+                String pos = token.get(CoreAnnotations.PartOfSpeechAnnotation.class);
+                String ne = token.get(CoreAnnotations.NamedEntityTagAnnotation.class);
+
+                System.out.println("Current word: " + word);
+                System.out.println("NER label: " + ne);
+                System.out.println("POS tag of token: " + pos);
+
+                res.add(ne);
+            }
+
+            Tree tree = sentence.get(TreeCoreAnnotations.TreeAnnotation.class);
+        }
+
+        return res;
     }
 }
