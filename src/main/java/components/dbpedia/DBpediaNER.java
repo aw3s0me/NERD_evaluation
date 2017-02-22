0b0f16eb7345297ad55498673c83b7b4e739dd6d
aw3s0me <akorovin00@gmail.com>
2017-02-22 16:23:58 +0100

    fixing dbpedia to work as a disambiguator

--- src/main/java/components/dbpedia/DBpediaNER.java
+++ src/main/java/components/dbpedia/DBpediaNER.java
@@ -1,6 +1,7 @@
 package components.dbpedia;
 
 import components.NER;
+import org.apache.http.client.utils.URIBuilder;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -9,10 +10,7 @@ import org.w3c.dom.NodeList;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
+import java.net.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,18 +18,24 @@ import java.util.List;
  * Created by akorovin on 21.02.2017.
  */
 public class DBpediaNER implements NER {
-    private final String serviceUrl = "http://spotlight.sztaki.hu:2222/rest/spot?text=";
+    private final static String SERVICE_URL = "http://spotlight.sztaki.hu:2222/rest/annotate";
+    private final static Double CONFIDENCE = 0.2;
+    private final static Integer SUPPORT = 20;
 
     public List<String> getEntities(String slide) {
-        String urlToSend;
+        URL url;
         try {
-            urlToSend = serviceUrl + URLEncoder.encode(slide, "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
+            url = new URIBuilder(SERVICE_URL)
+                    .addParameter("text", URLEncoder.encode(slide, "UTF-8"))
+                    .addParameter("confidence", String.valueOf(CONFIDENCE))
+                    .addParameter("support", String.valueOf(SUPPORT))
+                    .build().toURL();
+        } catch (UnsupportedEncodingException | MalformedURLException | URISyntaxException e) {
+            System.out.println("Could not create a link");
             return null;
         }
 
-        return sendUrl(urlToSend);
+        return sendUrl(url.toString());
     }
 
     private List<String> sendUrl(String url) {
@@ -43,6 +47,8 @@ public class DBpediaNER implements NER {
             URLConnection urlConnection = urlObj.openConnection();
             HttpURLConnection connection = null;
             connection = (HttpURLConnection) urlConnection;
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Accept", "text/xml");
 
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
@@ -50,7 +56,7 @@ public class DBpediaNER implements NER {
             Document doc = dBuilder.parse(connection.getInputStream());
             doc.getDocumentElement().normalize();
 
-            NodeList nList = doc.getElementsByTagName("surfaceForm");
+            NodeList nList = doc.getElementsByTagName("Resource");
 
             for (int temp = 0; temp < nList.getLength(); temp++) {
 
@@ -58,7 +64,7 @@ public class DBpediaNER implements NER {
 
                 if (nNode.getNodeType() == Node.ELEMENT_NODE) {
                     Element eElement = (Element) nNode;
-                    String text = eElement.getAttribute("name");
+                    String text = eElement.getAttribute("URI");
                     System.out.println(text);
                     retLst.add(text);
                 }
